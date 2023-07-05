eb3c85eddd8f050a3a5aff0165db2553d4480097
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-07-05 19:09:26 +0300

    created the file to handle forgot password

---
+++ password.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Forgot Password | stuStay</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="flex items-center justify-center h-screen bg-gray-900">
+    <div class="px-2">
+        <div class="flex flex-col items-center mb-2">
+            <img src="Screenshot 2023-07-01 195602.png" alt="" class="w-16">
+        </div>
+        <div class="border border-blue-500 shadow-md rounded-lg px-8 py-10">
+            <p class="text-gray-300 mb-6">Enter your email address below and we'll send you a password reset link.</p>
+            <form>
+                <div class="mb-6">
+                    <input class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" type="email" id="email" name="email" placeholder="Enter your email address" required>
+                </div>
+                <div class="flex items-center justify-end">
+                    <button class="bg-orange-700 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Reset Password</button>
+                </div>
+            </form>
+        </div>
+</body>
+</html>
+
+</body>
+</html>

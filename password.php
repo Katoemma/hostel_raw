ed9937a8957a48748005742da9dec5b1b54e7b22
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-07-08 20:59:55 +0300

    installed PHPmailer for password reset

--- password.php
+++ password.php
@@ -10,16 +10,17 @@
 <body class="flex items-center justify-center h-screen bg-gray-900">
     <div class="px-2">
         <div class="flex flex-col items-center mb-2">
-            <img src="Screenshot 2023-07-01 195602.png" alt="" class="w-16">
+            <img src="users/Screenshot 2023-07-01 195602.png" alt="" class="w-16">
+            <h1 class="text-white font-semibold text-4xl text-center">Stustay</h1>
         </div>
         <div class="border border-blue-500 shadow-md rounded-lg px-8 py-10">
-            <p class="text-gray-300 mb-6">Enter your email address below and we'll send you a password reset link.</p>
+            <p class="text-gray-300 mb-6">Enter valid email address to reset your password.</p>
             <form>
                 <div class="mb-6">
                     <input class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500" type="email" id="email" name="email" placeholder="Enter your email address" required>
                 </div>
                 <div class="flex items-center justify-end">
-                    <button class="bg-orange-700 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Reset Password</button>
+                    <button class="w-full md:w-auto bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Reset Password</button>
                 </div>
             </form>
         </div>

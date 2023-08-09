6dee5ca74b89d930f8dc0252ac22d28411b8b748
Katoemma <103442484+Katoemma@users.noreply.github.com>
2023-08-09 20:14:54 +0300

    modified the students listing

--- users/student/includes/header.php
+++ users/student/includes/header.php
@@ -101,7 +101,7 @@
                         <div class="absolute -top-3 -right-3 md:top-0 md:right-0 px-2 py-1.5 rounded-full bg-indigo-800 text-xs font-mono font-bold">23</div>
                     </div>
                 </a>
-                <a href="users.php" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group">
+                <a href="history.php" class="hover:bg-white/10 transition duration-150 ease-linear rounded-lg py-3 px-2 group">
                     <div class="flex flex-col space-y-2 md:flex-row md:space-y-0 space-x-2 items-center">
                         <div>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 group-hover:text-indigo-400">
@@ -109,7 +109,7 @@
                               </svg>                              
                         </div>
                         <div>
-                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4 group-hover:text-indigo-400">Requests</p>
+                            <p class="font-bold text-base lg:text-lg text-slate-200 leading-4 group-hover:text-indigo-400">Last Activity</p>
                         </div>
                         
                     </div>
--- users/system_Admin/students.php
+++ users/system_Admin/students.php
@@ -6,7 +6,7 @@
 <?php include('includes/header.php') ?>
 <div id="content" class="bg-white/10 col-span-9 rounded-lg p-6">
     <div>
-        <h1 class="font-bold py-4 uppercase">Hostels</h1>
+        <h1 class="font-bold py-4 uppercase">Students</h1>
         <!-- component -->
         <div class="md:px-32 py-8 w-full">
         <div class="shadow overflow-hidden rounded border-b border-gray-200">
@@ -21,21 +21,21 @@
             </thead>
             <tbody class="text-gray-700">
                 <?php foreach($students as $student):?>
-                    <?php $booking = selectAll('booking',['student'=>$student['id']]);?>
-                    <?php foreach ($booking as $key => $book):?>
-                        <?php $hostel = selectOne('hostels',['id'=>$book['hostel']]);?>
+                    <?php $booking = selectOne('booking',['student'=>$student['id']]);?>
+                    <?php $hostelic = selectOne('hostels',['id'=>$booking['hostel']]);?>
                                             
                     <tr>
                         <td class="w-1/3 text-left py-3 px-4"><?php echo $student['fname']." ".$student['lname'];?> </td>
-                        <?php if($book['status']==1):?>
-                            <td class="w-1/3 text-left py-3 px-4"><?php echo $hostel['name'];?></td>
+                        <?php if($booking != null || $hostelic != null):?>
+                            <?php if($booking['status']== 1):?>
+                                <td class="w-1/3 text-left py-3 px-4"><?php echo $hostelic['name'];?></td>
+                            <?php endif;?>
                         <?php else:?>
                             <td class="w-1/3 text-left py-3 px-4">Not Assigned</td>
                         <?php endif;?>
                         <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="tel:622322662"><?php echo $student['phone']?></a></td>
                         <td class="text-left py-3 px-4"><a class="hover:text-blue-500" href="mailto:jonsmith@mail.com"><?php echo $student['email']?></a></td>
                     </tr>
-                    <?php endforeach;?>
                 <?php endforeach;?>
             </tbody>
             </table>
